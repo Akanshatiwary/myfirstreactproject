f1cf5bd6fc809ef287aa9a001b58e173860a34a6
akansha <akanshatiwary03@gmail.com>
2020-08-07 19:15:14 +0530

    Updates

---
+++ precache-manifest.408edc02ebb61277961b7c5735082e96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db2a95efa0fb64540580f9f12acf719",
+    "url": "/Akanshatiwary/myfirstreactproject/index.html"
+  },
+  {
+    "revision": "96590787e91ed192e29e",
+    "url": "/Akanshatiwary/myfirstreactproject/static/css/main.4600ba9d.chunk.css"
+  },
+  {
+    "revision": "df5b1ffba174fd9a8300",
+    "url": "/Akanshatiwary/myfirstreactproject/static/js/2.953187bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Akanshatiwary/myfirstreactproject/static/js/2.953187bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96590787e91ed192e29e",
+    "url": "/Akanshatiwary/myfirstreactproject/static/js/main.cb879b85.chunk.js"
+  },
+  {
+    "revision": "ad345a33d4551f184b0f",
+    "url": "/Akanshatiwary/myfirstreactproject/static/js/runtime-main.dc3c25a6.js"
+  }
+]);
